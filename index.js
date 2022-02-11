d280786c4e39a496e1db0d7270935d0ead1446f1
anushabalabhadra <94507125+anushabalabhadra@users.noreply.github.com>
2022-02-11 16:10:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Table from "./Table";
+const App = () => {
+  return (
+    <>
+      <Table />
+    </>
+  );
+};
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+);
